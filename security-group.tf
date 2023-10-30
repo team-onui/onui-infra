8ba1edf878b4291665018fc50aa7b32e3de2e1ef
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-30 15:49:32 +0900

    ♻️ ::

--- security-group.tf
+++ security-group.tf
@@ -1,14 +1,6 @@
-resource "aws_security_group" "default" {
+resource "aws_security_group" "alb" {
   vpc_id = aws_vpc.vpc.id
   name = "alb-sg"
-
-  ingress {
-    from_port         = var.host_port
-    protocol          = "tcp"
-    to_port           = var.host_port
-    cidr_blocks       = ["0.0.0.0/0"]
-    ipv6_cidr_blocks  = ["::/0"]
-  }
 
   ingress {
     from_port         = 443
@@ -27,29 +19,61 @@ resource "aws_security_group" "default" {
   }
 
   tags = {
-    name = "2-2Admin"
+    Name = "onui-alb-sg"
+    name = "onui"
   }
 }
 
-resource "aws_security_group" "ecs_tasks" {
-  vpc_id = aws_vpc.vpc.id
-  name = "ecs-tasks-sg"
+resource "aws_security_group" "ec2" {
+  vpc_id        = aws_vpc.vpc.id
+  name          = "onui-server-sg"
 
   ingress {
-    from_port       = var.host_port
-    protocol        = "tcp"
-    to_port         = var.container_port
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port     = 0
-    protocol      = "-1"
-    to_port       = 0
-    cidr_blocks   = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  depends_on = [aws_security_group.alb]
+
   tags = {
-    name = "2-2Admin"
+    Name = "onui-server-sg"
+    name = "onui"
   }
 }
