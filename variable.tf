4695c8a07ccab35e5d73035463df6cd20b682e2f
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-30 14:20:22 +0900

    🔥 :: un used variable

--- variable.tf
+++ variable.tf
@@ -3,40 +3,17 @@ variable "region" {
   default = "ap-northeast-2"
 }
 
-variable "container_port" {
-  type    = number
-  default = 80
-}
-
-variable "host_port" {
-  type    = number
-  default = 80
-}
-
 variable "az_count" {
   type    = number
   default = 2
 }
 
-variable "scaling_max_capacity" {
-  type    = number
-  default = 3
-}
-
-variable "scaling_min_capacity" {
-  type    = number
-  default = 1
-}
-
-variable "cpu_or_memory_limit" {
-  type    = number
-  default = 70
-}
-
 variable "domain" {
+  type    = string
   default = "kanghyuk.co.kr"
 }
 
 variable "app_name" {
-  default = "highthon"
+  type    = string
+  default = "onui"
 }
