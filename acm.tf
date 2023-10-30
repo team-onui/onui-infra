10836f116dab777c1b1a12fa58216e50ca71aab8
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-30 15:48:32 +0900

    ♻️ ::

--- acm.tf
+++ acm.tf
@@ -1,11 +1,11 @@
 resource "aws_acm_certificate" "cert" {
-  domain_name       = var.domain
+  domain_name       = "*.${var.domain}"
   validation_method = "DNS"
   lifecycle {
     create_before_destroy = true
   }
   tags = {
-    name = "2-2Admin"
+    name = "onui"
   }
 }
 
